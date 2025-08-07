95904d149cb6fdc58bf92fc942e86240d3a372e9
codezzzahm1 <98508780+codezzzahm1@users.noreply.github.com>
2025-08-07 20:30:47 +0530

    Update urls.py

--- hana/urls.py
+++ hana/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     path('', include('sho.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
