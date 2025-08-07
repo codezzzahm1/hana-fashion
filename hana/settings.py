2e450ad1ca4c8658b3aacac31ea48f99b6eb7912
codezzzahm1 <98508780+codezzzahm1@users.noreply.github.com>
2025-08-07 23:55:46 +0530

    Update settings -newv.py

--- hana/settings.py
+++ hana/settings.py
@@ -69,8 +69,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# Use WhiteNoise’s storage backend to compress and version your files
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 
 STORAGES = {
@@ -82,7 +81,8 @@ STORAGES = {
     },
 }
 
-
+# Use WhiteNoise’s storage backend to compress and version your files
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'hana.urls'
 
@@ -166,6 +166,7 @@ LOGIN_REDIRECT_URL = '/'
 
 RAZORPAY_KEY_ID = 'rzp_test_iG0SjtY7Ls5zyP'
 RAZORPAY_KEY_SECRET = 'ppkv4BvBy4qcNZUsRAxTCD2E'
+
 
 
 
