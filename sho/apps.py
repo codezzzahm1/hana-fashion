326f902d01adabbe8294ed2bc5b85c0b0756579c
codezzzahm1 <98508780+codezzzahm1@users.noreply.github.com>
2025-08-07 19:17:45 +0530

    Delete sho directory

--- sho/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ShoConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'sho'
-
-    def ready(self):
-        import sho.signals 
